b6ed286dc4117a591eccc33703b7d12084894a4f
Yosyp Dobosh <joseph.dobosh@gmail.com>
2023-01-17 20:23:01 -0500

    create and populate player dropdown

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import { createLeagueDropDown} from "./scripts/league_dropdown.js";
 import { createTeamDropDown } from "./scripts/team_dropdown.js";
+import { createPlayerDropDown } from "./scripts/player_dropdown.js";
+import * as players from './data/data.json';
 
 document.addEventListener("DOMContentLoaded", function() {
     createLeagueDropDown();
     createTeamDropDown();
-    //updateTeamDropDown();
-    //updateTeamOptions();
+    //createPlayerDropDown();
+    //const teamSelect = document.getElementById('team-select');
+    //teamSelect.onchange = createPlayerDropDown;
+    createPlayerDropDown();
   });
---
+++ src/scripts/player_dropdown.js
@@ -0,0 +1,24 @@
+import * as players from '../data/data.json';
+
+export function createPlayerDropDown() {
+    const teamSelect = document.getElementById('team-select');
+    const playerSelect = document.getElementById('player-select');
+
+    teamSelect.onchange = function() {
+        // Clear the options of the third dropdown menu
+        playerSelect.innerHTML = '';
+
+        // Get the selected team
+        const selectedTeam = teamSelect.value;
+
+        // Populate the third dropdown menu with the players of the selected team
+        players.stats.forEach(player => {
+            if (player.Club.toLowerCase().replace(' ', '-') === selectedTeam) {
+                const option = document.createElement('option');
+                option.value = player.ID;
+                option.text = player.Name;
+                playerSelect.appendChild(option);
+            }
+        });
+    };
+}
