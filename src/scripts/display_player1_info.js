8948f484680ac02e82dbaa36143d8e97718307a9
Yosyp Dobosh <joseph.dobosh@gmail.com>
2023-01-19 14:18:11 -0500

    minor edits

--- src/scripts/display_player1_info.js
+++ src/scripts/display_player1_info.js
@@ -6,10 +6,11 @@ export function displayPlayer1Info() {
     playerSelect.addEventListener('change', function() {
         const playerData = players.stats.find(player => player.ID === playerSelect.value);
         
-        //document.getElementById('player1-photo').src = playerData.Photo;
+        document.getElementById('player1-photo').src = playerData.Photo;
         document.getElementById('player1-name').innerHTML = `Name: ${playerData.Name}`;
         document.getElementById('player1-age').innerHTML = `Age: ${playerData.Age}`;
         document.getElementById('player1-nationality').innerHTML = `Nationality: ${playerData.Nationality}`;
         document.getElementById('player1-position').innerHTML = `Position: ${playerData.Position}`;
+        document.getElementById('player1-foot').innerHTML = `Preferred foot: ${playerData["Preferred Foot"]}`;
     })
 }
--- src/scripts/display_player2_info.js
+++ src/scripts/display_player2_info.js
@@ -6,10 +6,12 @@ export function displayPlayer2Info() {
     playerSelect.addEventListener('change', function() {
         const playerData = players.stats.find(player => player.ID === playerSelect.value);
         
-        //document.getElementById('player2-photo').src = playerData.Photo;
+        document.getElementById('player2-photo').src = playerData.Photo;
         document.getElementById('player2-name').innerHTML = `Name: ${playerData.Name}`;
         document.getElementById('player2-age').innerHTML = `Age: ${playerData.Age}`;
         document.getElementById('player2-nationality').innerHTML = `Nationality: ${playerData.Nationality}`;
         document.getElementById('player2-position').innerHTML = `Position: ${playerData.Position}`;
+        document.getElementById('player2-foot').innerHTML = `Preferred foot: ${playerData["Preferred Foot"]}`;
+
     })
 }
